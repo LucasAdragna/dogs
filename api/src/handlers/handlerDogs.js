2cf257350a19649ae44ef210cadf40852349f947
Lucas Adragna <adragnajoaco@gmail.com>
2023-10-20 15:08:44 -0300

    intentando deploy

--- api/src/handlers/handlerDogs.js
+++ api/src/handlers/handlerDogs.js
@@ -3,10 +3,9 @@ const getRazas = require("../controllers/controllersDogs");
 const controllersName = require("../controllers/controllersName");
 
 const handlerDogs = async (req, res) => {
-
   const { name } = req.query;
 
-  const result = name ? await controllersName(name) : await getRazas();  // traigo por nombre, o todos los perros.
+  const result = name ? await controllersName(name) : await getRazas(); // traigo por nombre, o todos los perros.
 
   res.status(200).json(result);
 };
