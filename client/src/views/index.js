f7bcee4a20bc1383b1b6a78196e702643a74f10d
Lucas <126211954+LucasAdragna@users.noreply.github.com>
2023-10-20 15:18:18 -0300

    Delete client directory

--- client/src/views/index.js
+++
@@ -1,6 +0,0 @@
-import Landing from "./Landing/Landing";
-import Home from "./Home/home";
-import Form from "./Form/form";
-import Detail from "./Detail/detail";
-
-export { Landing, Home, Form, Detail };
