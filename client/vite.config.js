5564719f4aef2f448022318d56b13a27d819042d
Lucas Adragna <adragnajoaco@gmail.com>
2023-10-20 14:15:18 -0300

    Haciendo deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,5 +6,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://LucasAdragna.github.io/dogs ",
+  base: "https://LucasAdragna.github.io/dogs/ ",
 });
